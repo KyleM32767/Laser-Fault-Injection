dc1ff7217e77d1ce828d260fabfe3f24eeea1f26
Kyle Mitard <mitardk@gmail.com>
2023-01-23 13:40:25 -0500

    added python script for automatic test and program

---
+++ python_scripts/testandprogram.py
@@ -0,0 +1,54 @@
+'''
+	testandprogram.py
+
+	automatically runs the XOR gate test and reprograms the FPGA if needed
+
+	Author: Kyle Mitard
+	Created 
+'''
+
+import serial
+import os
+import time
+import sys
+
+# open port for gate tester
+tester = serial.Serial(port='COM6', baudrate=115200, timeout=3)
+time.sleep(2)
+
+# start test and get result
+tester.write(b'b')
+result = tester.readline()
+#print(result)
+
+# report on any faults
+nFaults = 0
+for i in range(0, len(result) - 2):
+
+	# report a fault if found
+	if result[i] == ord('F'):
+		nFaults += 1
+		print('fault at input', bin(i)[2:])
+	
+	# there are no other possible characters for faults
+	elif result[i] != ord('~'):
+		print('error in result')
+		exit()
+
+# reprogram FPGA if needed
+if nFaults == 0:
+	print('no faults')
+else:
+	print(nFaults, 'FAULTS! reprogramming FPGA...')
+	os.system('putty -load "vernam lab" -pw ' + sys.argv[1] + ' -m vivadoscript.txt -t') # something tells me storing the password in plaintext on github is a bad idea
+	print('done. verifying...')
+
+	# rerun test to see if it was reprogrammed successfully
+	tester.write(b'b')
+	result = tester.readline()
+	if result.find(ord('F')) == -1:
+		print('reprogram success.')
+	else:
+		print('reprogramming failed... perhaps the LUT is dead?')
+
+tester.close()
