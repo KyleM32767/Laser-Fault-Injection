9f4f496706c1e84ea9beb197d40a58f2246c51ec
Kyle Mitard <mitardk@gmail.com>
2023-01-26 11:30:43 -0500

    changed path to bitstream in auto program TCL script

--- tcl_scripts/autoprogram.tcl
+++ tcl_scripts/autoprogram.tcl
@@ -5,6 +5,6 @@ set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Digilent/2003
 open_hw_target
 current_hw_device [lindex [get_hw_devices] 0]
 refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
-set_property PROGRAM.FILE {/home/dev/kyle/xor_gate/xor_gate.runs/impl_1/xor_gate_top.bit} [lindex [get_hw_devices] 0]
+set_property PROGRAM.FILE {/home/dev/kyle/xor_gate_noRO/xor_gate_noRO.runs/impl_1/xor_gate__noRO_top.bit} [lindex [get_hw_devices] 0]
 program_hw_devices [lindex [get_hw_devices] 0]
 refresh_hw_device [lindex [get_hw_devices] 0]
